1749b71ebfb21cfbf71ef98725515ae66fe49209
Jigyass <90003678+Jigyass@users.noreply.github.com>
2023-09-29 23:49:58 -0500

    fixed merge two sorted lists

--- LinkedLists/MergeSortedLists.c
+++ LinkedLists/MergeSortedLists.c
@@ -1,30 +1,33 @@
-// Create a dummy node to serve as the head of the merged list
-    struct ListNode* dummy = malloc(sizeof(struct ListNode));
-    struct ListNode* current = dummy;
+//Merging two sorted linked lists to one sorted linked list
 
-    while (list1 != NULL && list2 != NULL) {
-        if (list1->val < list2->val) {
-            current->next = list1;
-            list1 = list1->next;
-        } else {
-            current->next = list2;
-            list2 = list2->next;
-        }
-        current = current->next;
-    }
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
+{
+	struct ListNode* new = malloc(sizeof(ListNode));
+	struct Node* prev = new;
 
-    // If one of the lists is not empty, append its remaining nodes to the merged list
-    if (list1 != NULL) {
-        current->next = list1;
-    }
-    if (list2 != NULL) {
-        current->next = list2;
-    }
-
-    // The merged list starts from the next of the dummy node
-    struct ListNode* merged_head = dummy->next;
-
-    // Free the dummy node (no longer needed)
-    free(dummy);
-
-    return merged_head;
+	while(list1 != NULL && list2 != NULL)
+	{
+		if(current->val <= current2->val)
+		{
+			prev->next = L1;
+			L1 = L1->next;
+		}
+		else()
+		{
+			prev->next = L2;
+			L2 = L2->next;
+		}
+		prev = prev->next;
+	}	
+	if(L1 == NULL)
+	{
+		prev->next = L2;
+	}
+	if(L2 == NULL)
+	{
+		prev->next = L1;
+	}
+	struct Node* result = new->next;
+	free(new);
+	return result;
+}
