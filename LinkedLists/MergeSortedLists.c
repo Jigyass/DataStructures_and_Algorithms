a5f52cab9479d1fbf1b5078be5e1ca374b1eb479
Jigyass <90003678+Jigyass@users.noreply.github.com>
2023-09-29 23:57:45 -0500

    Finally updated working code

--- LinkedLists/MergeSortedLists.c
+++ LinkedLists/MergeSortedLists.c
@@ -2,32 +2,35 @@
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
 {
-	struct ListNode* new = malloc(sizeof(ListNode));
-	struct Node* prev = new;
+	// Create a dummy node as the starting point of the merged list
+    
+	struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
+    	struct ListNode* prev = dummy;
+	// While both linked lists are not empty
+    	while (list1 != NULL && list2 != NULL) 
+	{
+        	if (list1->val <= list2->val) {
+            	prev->next = list1;
+            	list1 = list1->next;
+        	} 
+		else 
+		{
+			prev->next = list2;
+            		list2 = list2->next;
+        	}
+        	prev = prev->next;
+    	}
 
-	while(list1 != NULL && list2 != NULL)
-	{
-		if(current->val <= current2->val)
-		{
-			prev->next = L1;
-			L1 = L1->next;
-		}
-		else()
-		{
-			prev->next = L2;
-			L2 = L2->next;
-		}
-		prev = prev->next;
-	}	
-	if(L1 == NULL)
-	{
-		prev->next = L2;
-	}
-	if(L2 == NULL)
-	{
-		prev->next = L1;
-	}
-	struct Node* result = new->next;
-	free(new);
-	return result;
-}
+    // Once we reach the end of one linked list, append the other
+    if (list1 == NULL) {
+        prev->next = list2;
+    }
+    if (list2 == NULL) {
+        prev->next = list1;
+    }
+
+    // Return the sorted merged list starting from the node after the dummy node
+    struct ListNode* result = dummy->next;
+    free(dummy); // Free the dummy node
+    return result;
+    }
