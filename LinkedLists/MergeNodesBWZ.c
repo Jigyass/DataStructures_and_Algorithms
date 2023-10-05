78e84b7e9e5abe28985e6f46e3e6f7c5c1c0ca98
Jigyass <jigyassharma08@gmail.com>
2023-10-04 20:58:52 -0500

    Working code for MergeNodesBWZ.c

--- LinkedLists/MergeNodesBWZ.c
+++ LinkedLists/MergeNodesBWZ.c
@@ -1,18 +1,19 @@
 struct ListNode* mergeNodes(struct ListNode* head){
-    head=head->next;
-    struct ListNode* start=head;
-    while(start){
-        struct ListNode* end= start;  
+    head = head->next; //Because first node will always be zero
+    struct ListNode* current = head;
+    while(current != NULL)
+    {
         int sum=0;
-        while(end->val!=0)
-        { 
-            sum+= end->val; 
-            end=end->next;
+        struct ListNode* temp = current;
+        while(temp->val != 0)
+        {
+            sum+=temp->val;
+            temp = temp->next; 
         }
-        start->val=sum;   
-        start->next=end->next;   
-        start=start->next;   
-	}
+        current->val = sum;
+        current->next = temp->next;
+        current = current->next;
+    }
     return head;
 
 }
