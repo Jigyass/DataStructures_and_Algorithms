78d88e89fbcddbbc1f93da47286ac5a70fa4c98c
Jigyass <jigyassharma08@gmail.com>
2023-10-04 20:43:26 -0500

    fixed merge nodes btw zeroes.

--- LinkedLists/MergeNodesBWZ.c
+++ LinkedLists/MergeNodesBWZ.c
@@ -2,12 +2,16 @@ struct ListNode* mergeNodes(struct ListNode* head){
     head=head->next;
     struct ListNode* start=head;
     while(start){
-        struct ListNode* end= start;   /* Point to first node of current part for getting sum */
+        struct ListNode* end= start;  
         int sum=0;
-        while(end->val!=0) sum+= end->val , end=end->next;
-        start->val=sum;   /*assign sum to first node between two 0*/
-        start->next=end->next;   /*make this connect to first node of next part*/
-        start=start->next;    /*go..to..next..part*/
+        while(end->val!=0)
+        { 
+            sum+= end->val; 
+            end=end->next;
+        }
+        start->val=sum;   
+        start->next=end->next;   
+        start=start->next;   
 	}
     return head;
 
