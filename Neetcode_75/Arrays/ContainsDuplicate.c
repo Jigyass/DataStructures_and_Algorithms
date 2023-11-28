02e22b8d1b8e66afc89af5b773c5df7176d768ec
Jigyass <90003678+Jigyass@users.noreply.github.com>
2023-11-28 17:38:20 -0600

    Finished solving contains duplicate

---
+++ Neetcode_75/Arrays/ContainsDuplicate.c
@@ -0,0 +1,23 @@
+struct NumTable
+{
+        int val;
+        UT_hash_handle hh;
+};
+bool containsDuplicate(int* nums, int numsSize)
+{
+        struct NumTable *myTable = NULL, *tmp;
+        for(int i=0; i<numsSize; i++)
+        {
+                int num = nums[i];
+                HASH_FIND_INT(myTable, &num, tmp);
+                if(tmp)
+                {
+                        return 1;
+                }
+                tmp = malloc(sizeof(struct NumTable));
+                tmp->val = num;
+                HASH_ADD_INT(myTable, val, tmp);
+
+        }
+        return false;
+}
